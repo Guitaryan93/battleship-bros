0b3b4fa9bee42a5941bd24711d9762710e5d18a3
Ryan Woods <ryanwoodsmusic@gmail.com>
2024-12-27 14:20:21 -0400

    Removing commented code.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# from grid import Grid, Gridcell
 from game import Game
 from colorama import just_fix_windows_console
 
 if __name__ == "__main__":
     just_fix_windows_console()
     game = Game()
-    # game.grid.draw_grid()
